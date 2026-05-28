2a8a0e84df3d1ba0d9c30f69b38287f57eec0027
emmanuelalbe <emmanuelmalve@gmail.com>
2026-05-28 18:58:06 -0300

    Probando

--- backend/red-social/src/post/post.service.ts
+++ backend/red-social/src/post/post.service.ts
@@ -5,22 +5,22 @@ import { UpdatePostDto } from './dto/update-post.dto';
 @Injectable()
 export class PostService {
   create(createPostDto: CreatePostDto) {
-    return 'Esta accion crea una nueva publicacion';
+    return 'crear una nueva publicacion';
   }
 
   findAll() {
-    return 'Esta accion devuelve todas las publicaciones';
+    return 'todas las publicaciones';
   }
 
   findOne(id: number) {
-    return `Esta accion devuelve la publicacion #${id}`;
+    return ` devuelve la publicacion #${id}`;
   }
 
   update(id: number, updatePostDto: UpdatePostDto) {
-    return `Esta accion actualiza la publicacion #${id}`;
+    return `actualizar la publicacion #${id}`;
   }
 
   remove(id: number) {
-    return `Esta accion elimina la publicacion #${id}`;
+    return `eliminar la publicacion #${id}`;
   }
 }
